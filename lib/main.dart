20fad2ffb01319f72c24d2fd9e0d7e5ffa8cd4b9
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-04-13 20:16:01 +0530

    Added layoutbuilder

--- lib/main.dart
+++ lib/main.dart
@@ -147,7 +147,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     height: (MediaQuery.of(context).size.height -
                             appbar.preferredSize.height -
                             MediaQuery.of(context).padding.top) *
-                        0.3,
+                        1,
                     child: Chart(_recentTransactions),
                   )
                 : Container(
